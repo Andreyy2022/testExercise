d2bf28493e9ea3c9247cc8d24746bf71e5fdc863
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-23 15:24:05 +0300

    0.0.3

--- my-app/src/components/CanvasCircles.js
+++ my-app/src/components/CanvasCircles.js
@@ -3,19 +3,15 @@ import { useEffect, useRef } from "react"
 function CanvasCirtles() {
   useEffect(() => {
     const ctx1 = document.querySelector('canvas').getContext('2d');
-    ctx1.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 150, 0, getRadians(360) );
+    ctx1.beginPath();
+    ctx1.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 150, 0, 2 * Math.PI );
     ctx1.stroke();
 
     const ctx2 = document.querySelector('canvas').getContext('2d');
-    ctx2.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 250, 0, getRadians(360) );
+    ctx2.beginPath();
+    ctx2.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 250, 2 * Math.PI, 0 );
     ctx2.stroke();
-
-    function getRadians(degrees) {
-      return (Math.PI / 180) * degrees;
-    }
   }, []);
-
-  
 
     return (
       <div>
