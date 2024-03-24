5c8efcc8936be35d24ea9408ace534fb333e21bd
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-24 13:06:39 +0300

    0.0.3-2

--- my-app/src/components/CanvasCircles.js
+++ my-app/src/components/CanvasCircles.js
@@ -7,22 +7,22 @@ function CanvasCirtles() {
     ctx1.beginPath();
     ctx1.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 150, 0, 2 * Math.PI );
     ctx1.stroke();
-
+/*
+    let r = 150; // радиус
+    let amountOfPoints = 10; // количество точек
+    let x, y;
+    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints){
+        ctx1.beginPath();
+        x = window.screen.availWidth / 2 + r * Math.sin(i);
+        y = window.screen.availHeight / 2 + r * Math.cos(i);
+        ctx1.arc(x, y, 10, 0, 2 * Math.PI);
+        ctx1.closePath();
+    }
+*/
     const ctx2 = document.querySelector('#canvas').getContext('2d');
     ctx2.beginPath();
     ctx2.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 300, 2 * Math.PI, 0 );
     ctx2.stroke();
-  }, []);
-
-  useEffect(() => {
-    let r = 90; // радиус
-    let amountOfPoints = 7; // количество точек
-    let x, y;
-    let ctx = document.querySelector('#canvas').getContext('2d');
-    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints){
-        x = ctx.width / 2 + r * Math.sin(i);
-        y = ctx.height / 2 + r * Math.cos(i);
-    }
   }, []);
 
     return (
