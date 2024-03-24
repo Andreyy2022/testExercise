2abcf7f279be2af8b6593a1d0a0330293dd13f36
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2024-03-24 16:01:11 +0300

    0.0.4

--- my-app/src/components/CanvasCircles.js
+++ my-app/src/components/CanvasCircles.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 function CanvasCirtles() {
     
@@ -7,27 +7,40 @@ function CanvasCirtles() {
     ctx1.beginPath();
     ctx1.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 150, 0, 2 * Math.PI );
     ctx1.stroke();
-/*
-    let r = 150; // радиус
-    let amountOfPoints = 10; // количество точек
-    let x, y;
-    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints){
+
+    let r1 = 150; // радиус
+    let amountOfPoints1 = 10; // количество точек
+    let x1, y1;
+    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints1){
         ctx1.beginPath();
-        x = window.screen.availWidth / 2 + r * Math.sin(i);
-        y = window.screen.availHeight / 2 + r * Math.cos(i);
-        ctx1.arc(x, y, 10, 0, 2 * Math.PI);
+        x1 = window.screen.availWidth / 2 + r1 * Math.sin(i);
+        y1 = window.screen.availHeight / 2 + r1 * Math.cos(i);
+        ctx1.arc(x1, y1, 10, 0, 2 * Math.PI);
+        ctx1.stroke();
         ctx1.closePath();
     }
-*/
+
     const ctx2 = document.querySelector('#canvas').getContext('2d');
     ctx2.beginPath();
     ctx2.arc( window.screen.availWidth / 2, window.screen.availHeight / 2, 300, 2 * Math.PI, 0 );
     ctx2.stroke();
+
+    let r2 = 300; // радиус
+    let amountOfPoints2 = 29; // количество точек
+    let x2, y2;
+    for(let i = 0; i < Math.PI * 2; i += Math.PI * 2 / amountOfPoints2){
+        ctx1.beginPath();
+        x2 = window.screen.availWidth / 2 + r2 * Math.sin(i);
+        y2 = window.screen.availHeight / 2 + r2 * Math.cos(i);
+        ctx1.arc(x2, y2, 10, 0, 2 * Math.PI);
+        ctx1.stroke();
+        ctx1.closePath();
+    }
   }, []);
 
     return (
       <div>
-        <canvas id="canvas" width={1100} height={1100}></canvas>
+        <canvas id="canvas" width={window.screen.availWidth} height={window.screen.availWidth}></canvas>
       </div>
     );
 }
